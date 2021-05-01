0fdc154233853412bf3b6a85bc9580d26c7c14dd
兹克斯马奇 <71143103+zxmacc@users.noreply.github.com>
2021-05-01 09:58:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const Application = require('@waline/vercel');
+const Waline = require('@waline/vercel');
 
-module.exports = Application({
-  async postSave(comment) {
-    //do what ever you want after save comment
-  }
+module.exports = Waline({
+  forbiddenWords: [
+    '习近平',
+    '毛泽东',
+    '快递',
+    '空包',
+    '代发',
+    '单号'
+  ],
+  disallowIPList: [
+    '220.173.125.83',
+    '222.83.150.92',
+    '222.217.145.185',
+    '171.106.203.19',
+    '113.14.130.128',
+    '117.136.14.181',
+    '117.183.0.241'
+  ],
+  secureDomains: [
+    'www.zxmacc.cf',
+    'blog.zxmacc.cf',
+    'zxmacc.github.io'
+  ]
 });
